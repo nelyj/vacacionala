311767e68044fa39635de82bac8423663ad6a669
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-24 01:35:58 -0300

    remove scaffold stylesheet and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :places
-
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'places#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
