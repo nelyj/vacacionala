849ac9c5cda497461c3dd36c4fa8a9bfff5e068a
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-25 14:32:18 -0300

    Updated gemfile and modified the user controller and routes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   respond_to :html
 
   def index
